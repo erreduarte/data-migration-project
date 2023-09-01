43e22a82222db23443f5e1817beb6649e4ecd83f
Renan Duarte <114159104+erreduarte@users.noreply.github.com>
2023-09-01 09:18:51 +0100

    Update and rename sales.invoices to sales_invoices.sql

---
+++ sql-codes /sales_invoices.sql
@@ -0,0 +1,47 @@
+WITH ecommerce_invoices AS (
+  SELECT
+    a.receipt:id                                                       AS ID,
+    a.order_id,
+    a.created_at,
+    a.status,
+    CASE
+      WHEN kind = 'refund' THEN amount * -1::float
+      ELSE amount::float
+    END                                                               AS total_price,
+    CASE
+      WHEN kind = 'refund' THEN b.subtotal_price * -1::float
+      ELSE b.subtotal_price::float
+    END                                                               AS subtotal_price,
+    CASE
+      WHEN kind = 'refund' THEN b.total_tax * -1::float
+      ELSE b.total_tax::float
+    END                                                               AS taxes
+  FROM
+    {{ref('base_ecommerce_transactions')}} a
+  JOIN
+    {{ref('base_ecommerce_orders')}} b 
+    ON a.order_id = b.id
+),
+
+subscription_invoices AS (
+  SELECT
+    ID,
+    NULL                                                              AS order_id,
+    created_at,
+    state                                                             AS status,
+    total::float                                                      AS total_price,
+    subtotal::float                                                   AS subtotal_price,
+    tax::float                                                        AS taxes,
+    NULL                                                              AS shipping_cost
+  FROM
+    {{ref('base_subscription_invoices')}}
+),
+
+sales_invoices AS (
+  SELECT * FROM ecommerce_invoices
+  UNION ALL
+  SELECT * FROM subscription_invoices
+)
+
+SELECT * FROM sales_invoices;
+
